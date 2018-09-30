f873990287315b8093d92cb809ecc6197b44a33b
ZouJin <zoujin.admin@exlb.pw>
2018-09-30 17:14:02 +0800

    LinePutScript
    LinePutScript是一种定义行读取结构和描述其内容的标准语言
    本类库可以更轻松的创建,修改,保存LinePutScript

---
+++ LinePutScript/Class.cs
@@ -0,0 +1,364 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace LinePutScript
+{
+    /// <summary>
+    /// 子类 LinePutScript最基础的类
+    /// </summary>
+    public class Sub
+    {
+        /// <summary>
+        /// 创建一个子类
+        /// </summary>
+        public Sub() { }
+        /// <summary>
+        /// 通过lpsSub文本创建一个子类
+        /// </summary>
+        /// <param name="lpsSub">lpsSub文本</param>
+        public Sub(string lpsSub)
+        {
+            string[] st = lpsSub.Split('#');
+            Name = st[0];
+            if (st.Length > 1)
+                Info = st[1];
+        }
+        /// <summary>
+        /// 将其他Sub内容拷贝到本Sub
+        /// </summary>
+        /// <param name="sub">其他Sub</param>
+        public void Set(Sub sub)
+        {
+            Name = sub.Name;
+            Info = sub.Info;            
+        }
+
+        /// <summary>
+        /// 名称 没有唯一性
+        /// </summary>
+        public string Name;
+        /// <summary>
+        /// 信息
+        /// </summary>
+        public string Info = "";
+        ////暂时应该不需要判断类型
+        ///// <summary>
+        ///// 获取当前标签的类型
+        ///// </summary>
+        ///// <returns>类型</returns>
+        //public new virtual string GetType()
+        //{
+        //    return "sub";
+        //}
+        /// <summary>
+        /// 退回Info的转义文本
+        /// </summary>
+        /// <returns>info的转义文本</returns>
+        public string GetInfo()
+        {
+            return TextReplace(Info);
+        }
+        /// <summary>
+        /// 退回Info集合的转义文本
+        /// </summary>
+        /// <returns>info的转义文本</returns>
+        public string[] GetInfos()
+        {
+            string[] sts = Info.Split(',');
+            for (int i = 0; i < sts.Length; i++)
+                sts[i] = TextReplace(sts[i]);
+            return sts;
+        }
+
+        /// <summary>
+        /// 将文本进行转义处理
+        /// </summary>
+        /// <param name="Reptex">要转义的文本</param>
+        /// <returns>转义后的文本</returns>
+        public static string TextReplace(string Reptex)
+        {
+            Reptex = Reptex.Replace("/|", ":|");
+            Reptex = Reptex.Replace("/tab", "\t");
+            Reptex = Reptex.Replace("/n", "\n");
+            Reptex = Reptex.Replace("/id", "#");
+            Reptex = Reptex.Replace("/?", "//");
+            Reptex = Reptex.Replace("/!", "/");
+            return Reptex;
+        }
+        /// <summary>
+        /// 将文本进行反转义处理
+        /// </summary>
+        /// <param name="Reptex">要反转义的文本</param>
+        /// <returns>反转义后的文本</returns>
+        public static string TextDeReplace(string Reptex)
+        {
+            Reptex = Reptex.Replace(":|", "/|");
+            Reptex = Reptex.Replace("\t", "/tab");
+            Reptex = Reptex.Replace("\n", "/n");
+            Reptex = Reptex.Replace("#", "/id");
+            Reptex = Reptex.Replace("//", "/?");
+            Reptex = Reptex.Replace("/", "/!");
+            return Reptex;
+        }
+        /// <summary>
+        /// 将当前Sub转换成文本格式
+        /// </summary>
+        /// <returns>Sub的文本格式</returns>
+        public new string ToString()//不能继承
+        {
+            return TextDeReplace(Name) + '#' + TextDeReplace(Info) + ":|";
+        }
+
+
+    }
+    /// <summary>
+    /// 行 包含多个子类 继承自子类
+    /// </summary>
+    public class Line : Sub
+    {
+        /// <summary>
+        /// 创建一行
+        /// </summary>
+        public Line() { }
+        /// <summary>
+        /// 通过lpsLine文本创建一行
+        /// </summary>
+        /// <param name="lpsLine">lpsSub文本</param>
+        public Line(string lpsLine)
+        {
+            string[] sts = Regex.Split(Info, ":|", RegexOptions.IgnoreCase);
+
+            string[] st = sts[0].Split('#');//第一个
+            Name = st[0];
+            if (st.Length > 1)
+                Info = st[1];
+
+            Text = sts.Last();//最后一个
+
+            for(int i = 1; i < sts.Length - 1; i++)
+            {
+                Subs.Add(new Sub(sts[i]));
+            }
+        }
+        /// <summary>
+        /// 将其他Line内容拷贝到本Line
+        /// </summary>
+        /// <param name="line">其他line</param>
+        public void Set(Line line)
+        {
+            Name = line.Name;
+            Info = line.Info;
+
+            Text = line.Text;
+            Subs = line.Subs.ToList();
+        }
+
+
+        /// <summary>
+        /// 文本 在末尾没有结束行号的文本
+        /// </summary>
+        public string Text = "";
+        /// <summary>
+        /// 退回Text的转义文本
+        /// </summary>
+        /// <returns>Text的转义文本</returns>
+        public string GetText()
+        {
+            return TextReplace(Text);
+        }
+        /// <summary>
+        /// 子项目
+        /// </summary>
+        public List<Sub> Subs = new List<Sub>();
+
+
+
+        #region List操作
+        /// <summary>
+        /// 将指定的Sub添加到Subs列表的末尾。
+        /// </summary>
+        /// <param name="newSub">要添加的Sub</param>
+        public void AddSub(Sub newSub)
+        {
+            Subs.Add(newSub);
+        }
+        /// <summary>
+        /// 将指定Sub的元素添加到Subs的末尾。
+        /// </summary>
+        /// <param name="newSubs">要添加的多个Sub</param>
+        public void AddRange(params Sub[] newSubs)
+        {
+            Subs.AddRange(newSubs);
+        }
+
+        /// <summary>
+        /// 将指定的Sub添加到指定索引处
+        /// </summary>
+        /// <param name="index">应插入 Sub 的从零开始的索引</param>
+        /// <param name="newSub">要添加的Sub</param>
+        public void InsertSub(int index, Sub newSub)
+        {
+            Subs.Insert(index, newSub);
+        }
+        /// <summary>
+        /// 将指定Sub的元素添加指定索引处
+        /// </summary>
+        /// <param name="index">应插入 Sub 的从零开始的索引</param>
+        /// <param name="newSubs">要添加的多个Sub</param>
+        public void InsertRange(int index, params Sub[] newSubs)
+        {
+            Subs.InsertRange(index, newSubs);
+        }
+        /// <summary>
+        /// 从Subs中移除特定对象的第一个匹配项
+        /// </summary>
+        /// <param name="Sub">要从Subs中删除的Sub的名称</param>
+        /// <returns>如果成功移除了Sub，则为 true；否则为 false</returns>
+        public bool Remove(Sub Sub)
+        {
+            return Subs.Remove(Sub);
+        }
+        /// <summary>
+        /// 从Subs中移除特定名称的第一个匹配项
+        /// </summary>
+        /// <param name="SubName">要从Subs中删除的Sub的名称</param>
+        /// <returns>如果成功移除了Sub，则为 true；否则为 false</returns>
+        public bool Remove(string SubName)
+        {
+            for (int i = 0; i < Subs.Count; i++)
+            {
+                if (Subs[i].Name == SubName)
+                {
+                    Subs.RemoveAt(i);
+                    return true;
+                }
+            }
+            return false;
+        }
+        /// <summary>
+        /// 确定某Sub是否在Line集合中。
+        /// </summary>
+        /// <param name="sub">要在Line集合中定位的Sub</param>
+        /// <returns>如果在Line集合中找到sub，则为True；否则为false。</returns>
+        public bool Contains(Sub sub)
+        {
+            if (this == sub)
+                return true;
+            return Subs.Contains(sub);
+        }
+        /// <summary>
+        /// 确定某Sub是否在Line集合中
+        /// </summary>
+        /// <param name="subName">要在Line集合中定位的Sub的名字</param>
+        /// <returns>如果在Line集合中找到符合的名字，则为True；否则为false。</returns>
+        public bool Contains(string subName)
+        {
+            if (Name == subName)
+                return true;
+            return (Subs.FirstOrDefault(x => x.Name == subName) != null);
+        }
+        /// <summary>
+        /// 匹配拥有相同名称的sub的所有元素。
+        /// </summary>
+        /// <param name="subName">用于定义匹配的名称</param>
+        /// <returns>如果找到相同名称的sub，其中所有元素均与指定谓词定义的条件匹配，则为该数组；否则为一个空的Array</returns>
+        public Sub[] FindAll(string subName)
+        {
+            List<Sub> subs = new List<Sub>();
+            foreach (Sub su in Subs)
+                if (su.Name == subName)
+                    subs.Add(su);
+            return subs.ToArray();
+        }
+        /// <summary>
+        /// 搜索与指定名称，并返回整个Subs中的第一个匹配元素。
+        /// </summary>
+        /// <param name="subName">用于定义匹配的名称</param>
+        /// <returns>如果找到相同名称的第一个sub，则为该sub；否则为null。</returns>
+        public Sub Find(string subName)
+        {
+            return Subs.FirstOrDefault(x => x.Name == subName);
+        }
+
+        /// <summary>
+        /// 搜索相同名称的Sub，并返回整个Subs中第一个匹配的sub从零开始的索引。
+        /// </summary>
+        /// <param name="subName">用于定义匹配的名称</param>
+        /// <returns>如果找到相同名称的sub的第一个元素，则为该元素的从零开始的索引；否则为 -1</returns>
+        public int IndexOf(string subName)
+        {
+            for (int i = 0; i < Subs.Count; i++)
+            {
+                if (Subs[i].Name == subName)
+                    return i;
+            }
+            return -1;
+        }
+        /// <summary>
+        /// 搜索相同名称的Sub，并返回整个Sub中全部匹配的sub从零开始的索引。
+        /// </summary>
+        /// <param name="subName">用于定义匹配的名称</param>
+        /// <returns>如果找到相同名称的sub的元素，则为该元素的从零开始的索引组；否则为空的Array</returns>
+        public int[] IndexsOf(string subName)
+        {
+            List<int> lines = new List<int>();
+            for (int i = 0; i < Subs.Count; i++)
+            {
+                if (Subs[i].Name == subName)
+                    lines.Add(i);
+            }
+            return lines.ToArray();
+        }
+        #endregion
+
+        /// <summary>
+        /// 将当前Line转换成文本格式
+        /// </summary>
+        /// <returns>Line的文本格式</returns>
+        public new string ToString()//不能继承
+        {
+            StringBuilder str = new StringBuilder(TextDeReplace(Name) + '#' + TextDeReplace(Info) + ":|");
+            foreach (Sub su in Subs)
+                str.Append(su.ToString());
+            str.Append(TextDeReplace(Text));
+            return str.ToString();
+        }
+
+
+        ////暂时应该不需要判断类型
+        ///// <summary>
+        ///// 获取当前标签的类型
+        ///// </summary>
+        ///// <returns>类型</returns>
+        //public override string GetType()
+        //{
+        //    return "line";
+        //}
+    }
+
+    ////目前不清楚是否需要组类别 用户可以自行设定，没必要限制用户想象力
+    ///// <summary>
+    ///// 组 包含多个Line 继承自子类
+    ///// </summary>
+    //public class Group : Sub
+    //{
+    //    /// <summary>
+    //    /// 文本 在末尾没有结束Line号的文本
+    //    /// </summary>
+    //    public string Text = "";
+    //    /// <summary>
+    //    /// 子Line
+    //    /// </summary>
+    //    public List<Line> Lines = new List<Line>();
+    //    /// <summary>
+    //    /// 获取当前标签的类型
+    //    /// </summary>
+    //    /// <returns>类型</returns>
+    //    public override string GetType()
+    //    {
+    //        return "group";
+    //    }
+    //}  
+}
---
+++ LinePutScript/LpsDocument.cs
@@ -0,0 +1,354 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinePutScript
+{
+    /// <summary>
+    /// 文件 包括文件读写等一系列操作
+    /// </summary>
+    public class LpsDocument
+    {/// <summary>
+     /// 集合 全部文件的数据
+     /// </summary>
+        public List<Line> Assemblage = new List<Line>();
+
+        #region List操作
+        /// <summary>
+        /// 将指定的Line添加到Assemblage列表的末尾。
+        /// </summary>
+        /// <param name="newLine">要添加的Line</param>
+        public void AddLine(Line newLine)
+        {
+            Assemblage.Add(newLine);
+        }
+        /// <summary>
+        /// 将指定Line的元素添加到Assemblage的末尾。
+        /// </summary>
+        /// <param name="newLines">要添加的多个Line</param>
+        public void AddRange(params Line[] newLines)
+        {
+            Assemblage.AddRange(newLines);
+        }
+
+        /// <summary>
+        /// 将指定的Line添加到指定索引处
+        /// </summary>
+        /// <param name="index">应插入 Line 的从零开始的索引</param>
+        /// <param name="newLine">要添加的Line</param>
+        public void InsertLine(int index, Line newLine)
+        {
+            Assemblage.Insert(index, newLine);
+        }
+        /// <summary>
+        /// 将指定Line的元素添加指定索引处
+        /// </summary>
+        /// <param name="index">应插入 Line 的从零开始的索引</param>
+        /// <param name="newLines">要添加的多个Line</param>
+        public void InsertRange(int index, params Line[] newLines)
+        {
+            Assemblage.InsertRange(index, newLines);
+        }
+        /// <summary>
+        /// 从Assemblage中移除特定对象的第一个匹配项
+        /// </summary>
+        /// <param name="line">要从Assemblage中删除的Line的名称</param>
+        /// <returns>如果成功移除了line，则为 true；否则为 false</returns>
+        public bool Remove(Line line)
+        {
+            return Assemblage.Remove(line);
+        }
+        /// <summary>
+        /// 从Assemblage中移除特定名称的第一个匹配项
+        /// </summary>
+        /// <param name="lineName">要从Assemblage中删除的Line的名称</param>
+        /// <returns>如果成功移除了line，则为 true；否则为 false</returns>
+        public bool Remove(string lineName)
+        {
+            for (int i = 0; i < Assemblage.Count; i++)
+            {
+                if (Assemblage[i].Name == lineName)
+                {
+                    Assemblage.RemoveAt(i);
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// 从Assemblage中移除移除与条件相匹配的所有Line。
+        /// </summary>
+        /// <param name="line">要从Assemblage中删除的Line</param>
+        public void RemoveAll(Line line)
+        {
+            while (Assemblage.Remove(line)) ;
+        }
+        /// <summary>
+        /// 从Assemblage中移除移除与名称相匹配的所有Line。
+        /// </summary>
+        /// <param name="lineName">要从Assemblage中删除的Line的名称</param>
+        public void RemoveAll(string lineName)
+        {
+            for (int i = 0; i < Assemblage.Count; i++)
+            {
+                if (Assemblage[i].Name == lineName)
+                {
+                    Assemblage.RemoveAt(i--);
+                }
+            }
+        }
+        /// <summary>
+        /// 移除Assemblage的指定索引处的Line。
+        /// </summary>
+        /// <param name="index">要移除的Line的从零开始的索引</param>
+        public void RemoveAt(int index)
+        {
+            Assemblage.RemoveAt(index);
+        }
+
+        /// <summary>
+        /// 确定某Line是否在Assemblage中。
+        /// </summary>
+        /// <param name="line">要在Assemblage中定位的Line。</param>
+        /// <returns>如果在Assemblage中找到line，则为True；否则为false。 </returns>
+        public bool Contains(Line line)
+        {
+            return Assemblage.Contains(line);
+        }
+        /// <summary>
+        /// 确定某元素是否在Assemblage中。
+        /// </summary>
+        /// <param name="sub">要在Assemblage中定位的sub。</param>
+        /// <returns>如果在Assemblage中找到line，则为True；否则为false。</returns>
+        public bool Contains(Sub sub)
+        {
+            foreach (Line li in Assemblage)
+                if (li.Contains(sub))
+                    return true;
+            return false;
+        }
+        /// <summary>
+        /// 确定某Line(名字定位)是否在Assemblage中
+        /// </summary>
+        /// <param name="lineName">Line的名字</param>
+        /// <returns>如果在Assemblage中找到符合的名字，则为True；否则为false。</returns>
+        public bool ContainsLine(string lineName)
+        {
+            return (Assemblage.FirstOrDefault(x => x.Name == lineName) != null);
+        }
+        /// <summary>
+        /// 确定某sub(名字定位)是否在Assemblage中
+        /// </summary>
+        /// <param name="subName">sub的名字</param>
+        /// <returns>如果在Assemblage的sub中找到符合的名字，则为True；否则为false。</returns>
+        public bool ContainsSub(string subName)
+        {
+            if (Assemblage.FirstOrDefault(x => x.Name == subName) != null)
+                return true;
+            return Assemblage.FirstOrDefault(x => x.Contains(subName)) != null;
+            //foreach (Line li in Assemblage)
+            //    if (li.Contains(subName))
+            //        return true;
+            //return false;
+        }
+        /// <summary>
+        /// 匹配拥有相同名称的Line的所有元素。
+        /// </summary>
+        /// <param name="lineName">用于定义匹配的名称</param>
+        /// <returns>如果找到相同名称的Line，其中所有元素均与指定谓词定义的条件匹配，则为该数组；否则为一个空的Array</returns>
+        public Line[] FindAllLine(string lineName)
+        {
+            List<Line> lines = new List<Line>();
+            foreach (Line li in Assemblage)
+                if (li.Name == lineName)
+                    lines.Add(li);
+            return lines.ToArray();
+        }
+        /// <summary>
+        /// 搜索与指定名称，并返回整个Assemblage中的第一个匹配元素。
+        /// </summary>
+        /// <param name="lineName">用于定义匹配的名称</param>
+        /// <returns>如果找到相同名称的第一个Line，则为该Line；否则为null。</returns>
+        public Line FindLine(string lineName)
+        {
+            return Assemblage.FirstOrDefault(x => x.Name == lineName);
+        }
+        /// <summary>
+        /// 匹配拥有相同名称的Sub的所有元素。
+        /// </summary>
+        /// <param name="subName">用于定义匹配的名称</param>
+        /// <returns>如果找到相同名称的Sub，其中所有元素均与指定谓词定义的条件匹配，则为该数组；否则为一个空的Array</returns>
+        public Sub[] FindAllSub(string subName)
+        {
+            List<Sub> lines = new List<Sub>();
+            foreach (Line li in Assemblage)
+            {
+                if (li.Name == subName)
+                    lines.Add(li);
+                lines.AddRange(li.FindAll(subName));
+            }
+            return lines.ToArray();
+        }
+        /// <summary>
+        /// 搜索与指定名称，并返回整个Assemblage中的第一个匹配元素。
+        /// </summary>
+        /// <param name="subName">用于定义匹配的名称</param>
+        /// <returns>如果找到相同名称的第一个Sub，则为该Line；否则为null。</returns>
+        public Sub FindSub(string subName)
+        {
+            foreach (Line li in Assemblage)
+            {
+                if (li.Name == subName)
+                    return li;
+                if (li.Contains(subName))
+                    return li.Find(subName);
+            }
+            return null;
+        }
+        /// <summary>
+        /// 搜索相同名称的Line，并返回整个Assemblage中第一个匹配的Line从零开始的索引。
+        /// </summary>
+        /// <param name="lineName">用于定义匹配的名称</param>
+        /// <returns>如果找到相同名称的Line的第一个元素，则为该元素的从零开始的索引；否则为 -1</returns>
+        public int IndexOf(string lineName)
+        {
+            for (int i = 0; i < Assemblage.Count; i++)
+            {
+                if (Assemblage[i].Name == lineName)
+                    return i;
+            }
+            return -1;
+        }
+        /// <summary>
+        /// 搜索相同名称的Line，并返回整个Assemblage中全部匹配的Line从零开始的索引。
+        /// </summary>
+        /// <param name="lineName">用于定义匹配的名称</param>
+        /// <returns>如果找到相同名称的Line的元素，则为该元素的从零开始的索引组；否则为空的Array</returns>
+        public int[] IndexsOf(string lineName)
+        {
+            List<int> lines = new List<int>();
+            for (int i = 0; i < Assemblage.Count; i++)
+            {
+                if (Assemblage[i].Name == lineName)
+                    lines.Add(i);
+            }
+            return lines.ToArray();
+        }
+
+        #endregion
+
+        //就是行操作
+        #region LPT操作
+        int lineNode;
+        /// <summary>
+        /// 当前Line读取进度
+        /// </summary>
+        public int LineNode
+        {
+            get { return lineNode; }
+            set
+            {
+                if (value > Assemblage.Count)
+                    lineNode = Assemblage.Count;
+                else if (value < 0)
+                    lineNode = 0;
+                else
+                    lineNode = value;
+            }
+        }
+        /// <summary>
+        /// 读取当前Line 并且自动切换到下一Line
+        /// </summary>
+        /// <returns>当前Line</returns>
+        public Line ReadNext()
+        {
+            return Assemblage[LineNode++];
+        }
+        /// <summary>
+        /// 获取当前Line
+        /// </summary>
+        /// <returns>当前Line</returns>
+        public Line Read()
+        {
+            return Assemblage[LineNode];
+        }
+
+        /// <summary>
+        /// 将指定的Line添加到当前读取进度之后
+        /// </summary>
+        /// <param name="newline">要添加的Line</param>
+        public void Append(Line newline)
+        {
+            InsertLine(LineNode + 1, newline);
+        }
+
+       
+
+        /// <summary>
+        /// 将读取进度设置为0
+        /// </summary>
+        public void ReadReset()
+        {
+            LineNode = 0;
+        }
+        /// <summary>
+        /// 将读取进度设置为上限
+        /// </summary>
+        public void ReadEnd()
+        {
+            LineNode = Assemblage.Count;
+        }
+
+        /// <summary>
+        /// 获取最后一行的信息
+        /// </summary>
+        /// <returns>最后一行的信息</returns>
+        public Line LastLine()
+        {
+            return Assemblage.Last();
+        }
+
+        #endregion
+
+
+
+        /// <summary>
+        /// 从指定的字符串加载LPS文档
+        /// </summary>
+        /// <param name="lps">包含要加载的LPS文档的字符串。</param>
+        /// <returns></returns>
+        public bool Load(string lps)
+        {
+            Assemblage.Clear();//清空当前文档
+            try
+            {
+                string[] file = lps.Replace("\r", "").Split('\n');
+                foreach (string str in file)
+                {
+                    Assemblage.Add(new Line(str));
+                }
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// 将当前Line转换成文本格式
+        /// </summary>
+        /// <returns>Line的文本格式</returns>
+        public new string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (Line li in Assemblage)
+                sb.Append(li.ToString());
+            return sb.ToString();
+        }
+    }
+
+}
