094293c9f26d5455e1d8f0d156a3eeaf550ca85a
ZouJin <zoujin.admin@exlb.pw>
2019-07-20 00:33:49 +0800

    LPS数据库
    LPS数据库 可以在多个程序中通过内存进行共享数据
    比传统的IO共享数据速度更快

---
+++ LinePutScriptDataBase/Class.cs
@@ -0,0 +1,221 @@
+﻿using LinePutScript;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.IO.MemoryMappedFiles;
+
+namespace LinePutScriptDataBase
+{
+    /// <summary>
+    /// 数据库
+    /// </summary>
+    public class DataBase
+    {
+        /// <summary>
+        /// 库名
+        /// </summary>
+        public string Name;
+        /// <summary>
+        /// 数据文件
+        /// </summary>
+        public LpsDocument LPS;
+        public DataBase(string name)
+        {
+            Name = name;
+            LPS = new LpsDocument($"DataBase#{name}:|ver#1:|capacity#1048576:|automapping#1:|");
+            DBL = new DataBaseLocker(this);
+        }
+        public DataBase(string name, string lps)
+        {
+            Name = name;
+            LPS = new LpsDocument(lps);
+            if (LPS.First() == null || LPS.First().Name.ToLower() != "database")
+                LPS.AddLine(new Line($"DataBase#{name}:|ver#1:|capacity#1048576:|automapping#1:|"));
+            DBL = new DataBaseLocker(this);
+        }
+        public DataBase(string name, FileInfo lpsdb)
+        {
+            Name = name;
+            //强制UTF-8
+            StreamReader sr = new StreamReader(lpsdb.OpenRead(), Encoding.UTF8);
+            string lps = sr.ReadToEnd();
+            sr.Close();
+            sr.Dispose();
+            LPS = new LpsDocument(lps);
+            if (LPS.First() == null || LPS.First().Name.ToLower() != "database")
+                LPS.AddLine(new Line($"DataBase#{name}:|ver#1:|capacity#1048576:|automapping#1:|"));
+            DBL = new DataBaseLocker(this);
+        }
+
+        /// <summary>
+        /// 映射到内存的数据库
+        /// </summary>
+        public DataBaseLocker DBL;
+
+
+        //设置内容
+
+        /// <summary>
+        /// 分配的内存容量
+        /// </summary>
+        public long Capacity
+        {
+            get
+            {
+                Sub sb = LPS.First().Find("capacity");
+                if (sb == null || !long.TryParse(sb.info, out long ot))
+                {
+                    LPS.First().AddSub(new Sub("capacity", "1048576"));
+                    return 1048576;//默认分配1mb
+                }
+                return ot;
+            }
+            set
+            {
+                Sub sb = LPS.First().Find("capacity");
+                if (sb == null)
+                {
+                    LPS.First().AddSub(new Sub("capacity", value.ToString()));
+                }
+                else
+                    sb.info = value.ToString();
+            }
+        }
+        /// <summary>
+        /// 是否自动部署
+        /// </summary>
+        public bool AutoMapping
+        {
+            get
+            {
+                Sub sb = LPS.First().Find("automapping");
+                if (sb == null)
+                {
+                    LPS.First().AddSub(new Sub("automapping", "1"));
+                    return true;//默认自动部署
+                }
+                return sb.info != "0";
+            }
+            set
+            {
+                Sub sb = LPS.First().Find("automapping");
+                if (sb == null)
+                {
+                    LPS.First().AddSub(new Sub("capacity", (value ? "1" : "0")));
+                }
+                else
+                    sb.info = (value ? "1" : "0");
+            }
+        }
+        /// <summary>
+        /// 是否自动备份
+        /// </summary>
+        public bool AutoBackup
+        {
+            get
+            {
+                Sub sb = LPS.First().Find("backup");
+                if (sb == null)
+                {
+                    LPS.First().AddSub(new Sub("backup", "1"));
+                    return true;//默认自动备份
+                }
+                return sb.info != "0";
+            }
+            set
+            {
+                Sub sb = LPS.First().Find("backup");
+                if (sb == null)
+                {
+                    LPS.First().AddSub(new Sub("backup", (value ? "1" : "0")));
+                }
+                else
+                    sb.info = (value ? "1" : "0");
+            }
+        }
+        /// <summary>
+        /// 读取密码密码,为空则未设置
+        /// </summary>
+        public string Password
+        {
+            get
+            {
+                Sub sb = LPS.First().Find("password");
+                if (sb == null)
+                {
+                    return "";//默认未设置
+                }
+                return sb.Info;
+            }
+            set
+            {
+                Sub sb = LPS.First().Find("password");
+                if (sb == null)
+                {
+                    LPS.First().AddSub(new Sub("capacity",value));
+                }
+                else
+                    sb.Info = value;
+            }
+        }
+        //映射内容
+
+        private bool mapping = false;
+        public bool Mapping
+        {
+            get => mapping;
+        }
+        /// <summary>
+        /// 映射数据库到内存中
+        /// </summary>
+        public void MaptoMemory()
+        {
+            if (mapping)
+                return;
+            MemoryMappedFile mmf = MemoryMappedFile.CreateNew("lpsdb" + Name, Capacity, MemoryMappedFileAccess.ReadWrite);
+            var viewAccessor = mmf.CreateViewAccessor();
+            viewAccessor.Write(0,ref DBL);
+            mapping = true;
+        }
+        /// <summary>
+        /// 关闭映射数据库
+        /// </summary>
+        public void Mapoff()
+        {
+            if (!mapping)
+                return;
+            MemoryMappedFile.OpenExisting("lpsdb" + Name).Dispose();
+            mapping = false;
+        }
+        
+
+    }
+
+    /// <summary>
+    /// 映射在内存的数据库
+    /// </summary>
+    public struct DataBaseLocker
+    {
+        private DataBase DB;
+        /// <summary>
+        /// 获取储存在其中的DataBase 密码错误则退回null
+        /// </summary>
+        /// <param name="password">密码,未设置则为空即可</param>
+        /// <returns></returns>
+        public DataBase GetDataBase(string password = "")
+        {
+            if (DB == null)
+                return null;
+            if (password == DB.Password)
+                return DB;
+            return null;
+        }
+        public DataBaseLocker(DataBase db)
+        {
+            DB = db;
+        }
+    }
+
+}
---
+++ LinePutScriptDataBase/LPSDBHost.cs
@@ -0,0 +1,294 @@
+﻿using LinePutScript;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace LinePutScriptDataBase
+{
+    /// <summary>
+    /// 主机
+    /// </summary>
+    public class LPSDBHost
+    {
+        /// <summary>
+        /// 主机数据库位置
+        /// </summary>
+        public readonly string PathMain;
+        /// <summary>
+        /// 数据库数据
+        /// </summary>
+        public List<DataBase> DataBases = new List<DataBase>();
+
+        /// <summary>
+        /// 配置
+        /// </summary>
+        public static Config CONFIG;
+
+        /// <summary>
+        /// 备份和储存共用的timer
+        /// </summary>
+        public System.Timers.Timer timer;
+        private int backuptime;
+
+        /// <summary>
+        /// 从路径启动数据库主机
+        /// </summary>
+        /// <param name="path">路径:数据库储存位置</param>
+        public LPSDBHost(string path)
+        {
+            PathMain = path;
+
+            //从文件导入数据库
+            DirectoryInfo di = new DirectoryInfo(path);
+            foreach (FileInfo fi in di.EnumerateFiles().Where(x => x.Extension.ToLower() == ".lpsdb"))
+            {
+                DataBases.Add(new DataBase(fi.Name.Split(new char[] { ' ' }, 2)[0].ToUpper()));
+            }
+            //获取配置文件
+            DataBase tmp = DataBases.Find(x => x.Name == "CONFIG");
+            if (tmp == null)
+            {
+                CONFIG = new Config();
+                DataBases.Add(CONFIG.DB);
+            }
+            else
+                CONFIG = new Config(tmp);
+
+            if (CONFIG.AutoMapping)
+                MaptoMemoryAuto();
+
+            timer = new System.Timers.Timer();
+            timer.Interval = CONFIG.AutoSaveTime;
+            backuptime = CONFIG.AutoBackupTime;
+        }
+
+        /// <summary>
+        /// 自动备份和自动储存计时器
+        /// </summary>
+        public void Timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            backuptime -= CONFIG.AutoSaveTime;
+            if (backuptime <= 0)
+            {
+                backuptime = CONFIG.AutoBackupTime;
+                BackUpAuto();
+            }
+            SaveALL();
+        }
+
+        /// <summary>
+        /// 映射全部自动启动数据库
+        /// </summary>
+        public void MaptoMemoryAuto()
+        {
+            foreach (DataBase db in DataBases.FindAll(x => x.AutoMapping))
+            {
+                db.MaptoMemory();
+            }
+        }
+        /// <summary>
+        /// 映射全部数据库
+        /// </summary>
+        public void MaptoMemoryALL()
+        {
+            foreach (DataBase db in DataBases)
+            {
+                db.MaptoMemory();
+            }
+        }
+        /// <summary>
+        /// 关闭映射全部数据库
+        /// </summary>
+        public void MapOffALL()
+        {
+            foreach (DataBase db in DataBases)
+            {
+                db.Mapoff();
+            }
+        }
+
+        /// <summary>
+        /// 进行备份操作
+        /// </summary>
+        public void BackUp(DataBase db)
+        {
+            DirectoryInfo di = new DirectoryInfo(PathMain + '\\' + db.Name);
+            if (!di.Exists)
+            {
+                di.Create();//如果目录不存在则创建
+            }
+            FileInfo fi = new FileInfo(PathMain + '\\' + db.Name + '\\' + DateTime.Now.ToShortDateString() + ' ' + DateTime.Now.ToShortTimeString() + ".lpsdb");
+            FileStream fs = fi.Create();
+            byte[] buff = Encoding.UTF8.GetBytes(db.LPS.ToString());
+            fs.Write(buff, 0, buff.Length);
+            fs.Close();
+            fs.Dispose();
+        }
+        /// <summary>
+        /// 备份全部自动备份数据库
+        /// </summary>
+        public void BackUpAuto()
+        {
+            foreach (DataBase db in DataBases.FindAll(x => x.AutoBackup))
+            {
+                BackUp(db);
+            }
+        }
+        /// <summary>
+        /// 进行存档操作
+        /// </summary>
+        public void Save(DataBase db)
+        {
+            FileInfo fi = new FileInfo(PathMain + '\\' + db.Name + ".lpsdb");
+            FileStream fs = fi.Create();
+            byte[] buff = Encoding.UTF8.GetBytes(db.LPS.ToString());
+            fs.Write(buff, 0, buff.Length);
+            fs.Close();
+            fs.Dispose();
+        }
+        /// <summary>
+        /// 存档全部数据库
+        /// </summary>
+        public void SaveALL()
+        {
+            foreach (DataBase db in DataBases)
+            {
+                Save(db);
+            }
+        }
+        /// <summary>
+        /// 修改数据库名
+        /// </summary>
+        /// <param name="db">数据库</param>
+        /// <param name="NewName">新名称:仅大写+下划线</param>
+        public void ReName(DataBase db,string NewName)
+        {
+            NewName = NewName.ToUpper();
+            if (db.Mapping)
+                return;
+            //首先先改文件
+            FileInfo fi = new FileInfo(PathMain + '\\' + db.Name + ".lpsdb");
+            fi.MoveTo(fi.DirectoryName + '\\' + NewName + ".lpsdb");
+            DirectoryInfo di = new DirectoryInfo(PathMain + '\\' + db.Name);
+            if (di.Exists)
+            {
+                di.MoveTo(PathMain + '\\' + NewName);
+            }
+            //然后改文件名
+            db.Name = NewName;
+            db.LPS.First().First().info = NewName;
+        }
+    }
+
+    /// <summary>
+    /// 配置文件
+    /// </summary>
+    public class Config
+    {
+        /// <summary>
+        /// 配置文件RAW
+        /// </summary>
+        public DataBase DB;
+
+        /// <summary>
+        /// 无配置文件时默认设置
+        /// </summary>
+        public Config()
+        {
+            DB = new DataBase("CONFIG");
+            DB.AutoMapping = false;//不自动映射配置文件
+        }
+        /// <summary>
+        /// 有配置文件
+        /// </summary>
+        public Config(DataBase db)
+        {
+            DB = db;
+        }
+        /// <summary>
+        /// 是否自动部署
+        /// </summary>
+        public bool AutoMapping
+        {
+            get
+            {
+                Line li = DB.LPS.FindLine("automapping");
+                if (li == null)
+                {
+                    DB.LPS.AddLine(new Line("automapping", "1"));
+                    return true;//默认自动部署
+                }
+                return li.info != "0";
+            }
+            set
+            {
+                Line li = DB.LPS.FindLine("automapping");
+                if (li == null)
+                {
+                    DB.LPS.AddLine(new Line("automapping", (value ? "1" : "0")));
+                }
+                else
+                    li.info = (value ? "1" : "0");
+            }
+        }
+
+        /// <summary>
+        /// 自动储存时间间隔
+        /// </summary>
+        public int AutoSaveTime
+        {
+            get
+            {
+                Line li = DB.LPS.FindLine("autosavetime");
+                if (li == null || !int.TryParse(li.info, out int ot))
+                {
+                    DB.LPS.AddLine(new Line("autosavetime", "3600000"));
+                    return 3600000;//默认1小时一次刷新
+                }
+                return ot;
+            }
+            set
+            {
+                Line li = DB.LPS.FindLine("autosavetime");
+                if (li == null)
+                {
+                    DB.LPS.AddLine(new Line("autosavetime", value.ToString()));
+                }
+                else
+                    li.info = value.ToString();
+            }
+        }
+        /// <summary>
+        /// 自动备份时间间隔
+        /// </summary>
+        public int AutoBackupTime
+        {
+            get
+            {
+                Line li = DB.LPS.FindLine("autobackuptime");
+                if (li == null || !int.TryParse(li.info, out int ot))
+                {
+                    DB.LPS.AddLine(new Line("autobackuptime", "3600000"));
+                    return 86400000;//默认1天备份一次
+                }
+                return ot;
+            }
+            set
+            {
+                Line li = DB.LPS.FindLine("autobackuptime");
+                if (li == null)
+                {
+                    DB.LPS.AddLine(new Line("autobackuptime", value.ToString()));
+                }
+                else
+                    li.info = value.ToString();
+            }
+        }
+    }
+
+
+
+
+}
